da68c18edde86e2d5d6c081a1f16f63c75e7e3c2
Ily3s <ilyes.bennanii@gmail.com>
2024-12-22 16:23:25 +0100

    cleaner day21

--- day21.cpp
+++ day21.cpp
@@ -14,102 +14,48 @@ using namespace std;
 
 string codes[5];
 
-// char -> coordinates(x, y)
-pair<int, int> numpad[256];
-pair<int, int> dirpad[256];
+struct Pad
+{
+    pair<int, int> char2coord[256];
+    char coord2char[4][3];
+
+    Pad(char coord2char[4][3]);
+};
+
+// zeroes are "forbidden" emplacements
+char raw_numpad[][3]{
+    {'7', '8', '9'},
+    {'4', '5', '6'},
+    {'1', '2', '3'},
+    {0, '0', 'A'}
+};
+char raw_dirpad[][3]{
+    {0, '^', 'A'},
+    {'<', 'v', '>'},
+    {0, 0, 0},
+    {0, 0, 0}
+};
+
+Pad numpad{raw_numpad};
+Pad dirpad{raw_dirpad};
+
+// compute the cost to go from a to b and press the button
+int64_t compute_cost(char a, char b, int k, Pad& pad);
+
+// mem[a][b][k] store the result of compute_cost(a, b, k, ..)
 int64_t mem[256][256][26];
 
-int64_t cost_dir(char a, char b, int k) {
-    auto [ia, ja] = dirpad[a]; auto [ib, jb] = dirpad[b];
-    if (k==1) return abs(ia-ib)+abs(ja-jb)+1;
-    if (mem[a][b][k] != -1) return mem[a][b][k];
-    int chari = ia > ib ? '^' : 'v';
-    int charj = ja > jb ? '<' : '>';
-    int di = abs(ia-ib), dj = abs(ja-jb);
-    int ii = 0, ij = 0;
-    int64_t cost1 = 0, cost2 = 0;
-    for (;ii<di; ii++) {
-        if (ia==1 && ja==0 && !ii) {
-            cost1 += cost_dir('A', charj, k-1);
-            cost1 += cost_dir(charj, chari, k-1);
-            ij++;
-            continue;
-        }
-        cost1 += cost_dir(ii?chari:'A', chari, k-1);
-    }
-    int v = ij;
-    for (;ij<dj; ij++) {
-        cost1 += cost_dir((ij!=v)?charj:di?chari:'A', charj, k-1);
-    }
-    cost1 += cost_dir(v<dj?charj:di?chari:'A', 'A', k-1);
-    ii = ij = 0;
-    for (;ij<dj; ij++) {
-        if (ia == 0 && jb==0 && ja-(ij+1) == 0) {
-            cost2 += cost_dir(ij?charj:'A', chari, k-1);
-            cost2 += cost_dir(chari, charj, k-1);
-            ii++;
-            continue;
-        }
-        cost2 += cost_dir(ij?charj:'A', charj, k-1);
-    }
-    v = ii;
-    for (;ii<di; ii++) {
-        cost2 += cost_dir((ii!=v)?chari:dj?charj:'A', chari, k-1);
-    }
-    cost2 += cost_dir(v<di?chari:dj?charj:'A', 'A', k-1);
-    mem[a][b][k] = min(cost1, cost2);
-    return mem[a][b][k];
-}
-
-int64_t cost_num(char a, char b, int robot_number) {
-    if (a==b) return 1;
-    auto [ia, ja] = numpad[a]; auto [ib, jb] = numpad[b];
-    int chari = ia > ib ? '^' : 'v';
-    int charj = ja > jb ? '<' : '>';
-    int di = abs(ia-ib), dj = abs(ja-jb);
-    vector<string> possibilities;
-    char* poss = new char[di+dj+1];
-    poss[di+dj] = '\0';
-    for (int k = 0; k < 1<<(di+dj); k++) {
-        int nbi = 0, x = ja, y = ia;
-        bool forbiden = false;
-        for (int i = 0; i < di+dj; i++) {
-            poss[i] = k&(1<<i) ? chari : charj;
-            if (k&(1<<i))
-                y += ia<ib ? 1 : -1;
-            else
-                x += ja<jb ? 1 : -1;
-            if (x == 0 && y == 3) forbiden = true;
-            nbi += !!(k&(1<<i));
-        }
-        if (nbi == di && !forbiden) possibilities.push_back(poss);
-    }
-    auto res = vector<int64_t>(possibilities.size(), 0);
-    for (int i = 0; i < possibilities.size(); i++) {
-        res[i] += cost_dir('A', possibilities[i][0], robot_number);
-        for (int j = 1; j < possibilities[i].size(); j++)
-            res[i] += cost_dir(possibilities[i][j-1], possibilities[i][j], robot_number);
-        res[i] += cost_dir(possibilities[i].back(), 'A', robot_number);
-    }
-    auto it = min_element(res.begin(), res.end());
-
-    delete[] poss;
-    return *it;
-}
-
-int64_t solve(int bot_number) {
-    for (int k = 0; k < 26; k++) {
-        for (int i = 256; i--;) {
-            for (int j = 256; j--;) mem[i][j][k] = -1;
-        }
-    }
+int64_t solve(int bot_number)
+{
+    // set/reset mem
+    for (int k = 256*256*26; k--;) ((int64_t*)mem)[k] = -1;
 
     int64_t output = 0;
 
     for (string& code : codes) {
-        int64_t cost = cost_num('A', code[0], bot_number);
+        int64_t cost = compute_cost('A', code[0], bot_number, numpad);
         for (int i = 1; i < code.size(); i++)
-            cost += cost_num(code[i-1], code[i], bot_number);
+            cost += compute_cost(code[i-1], code[i], bot_number, numpad);
         cost *= strtoll(code.c_str(), NULL, 10);
         output += cost;
     }
@@ -126,19 +72,71 @@ int main(int argc, char** argv)
     for (int i = 0; i<5 && getline(file, codes[i]); i++)
         if (codes[i].back() == '\r') codes[i].pop_back();
 
-    // init numpad and dirpad
-    numpad['0'] = {3, 1};
-    numpad['A'] = {3, 2};
-    for (int i = 1; i <= 9; i++)
-        numpad['0'+i] = {2-(i-1)/3, (i-1)%3};
-    dirpad['A'] = {0, 2};
-    dirpad['^'] = {0, 1};
-    dirpad['<'] = {1, 0};
-    dirpad['v'] = {1, 1};
-    dirpad['>'] = {1, 2};
-
     cout << "part1: " << solve(2) << endl;
     cout << "part2: " << solve(25) << endl;
 
     return 0;
+}
+
+Pad::Pad(char coord2char[4][3]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            this->coord2char[i][j] = coord2char[i][j];
+            if (coord2char[i][j])
+                char2coord[coord2char[i][j]] = {i, j};
+        }
+    }
+}
+
+void update_coords(int* i, int* j, char c) {
+    switch(c) {
+        case '<': (*j)--; break;
+        case '>': (*j)++; break;
+        case 'v': (*i)++; break;
+        case '^': (*i)--; break;
+    }
+}
+
+int64_t compute_cost(char a, char b, int k, Pad& pad)
+{
+    auto [ia, ja] = pad.char2coord[a];
+    auto [ib, jb] = pad.char2coord[b];
+
+    if (!k) return abs(ia-ib)+abs(ja-jb)+1;
+    if (mem[a][b][k] != -1) return mem[a][b][k];
+    if (a == b) return 1;
+
+    int chari = ia > ib ? '^' : 'v';
+    int charj = ja > jb ? '<' : '>';
+    int di = abs(ia-ib), dj = abs(ja-jb);
+
+    // build all legal set of moves that moves the bot's arm
+    // from a to b directly (without going backwards or anything)
+    vector<string> possibilities;
+    string poss(di+dj, 0);
+    for (int k = 0; k < 1<<(di+dj); k++) {
+        int nbi = 0, x = ja, y = ia;
+        bool forbiden = false;
+        for (int i = 0; i < di+dj; i++) {
+            poss[i] = k&(1<<i) ? chari : charj;
+            update_coords(&y, &x, poss[i]);
+            forbiden |= !pad.coord2char[y][x];
+            nbi += !!(k&(1<<i));
+        }
+        if (nbi == di && !forbiden) possibilities.push_back(poss);
+    }
+
+    // recursively compute the cost of each of these possibilities
+    auto results = vector<int64_t>(possibilities.size(), 0);
+    for (int i = 0; i < possibilities.size(); i++) {
+        results[i] += compute_cost('A', possibilities[i][0], k-1, dirpad);
+        for (int j = 1; j < possibilities[i].size(); j++)
+            results[i] += compute_cost(possibilities[i][j-1], possibilities[i][j], k-1, dirpad);
+        results[i] += compute_cost(possibilities[i].back(), 'A', k-1, dirpad);
+    }
+
+    // result is the minimum
+    auto it = min_element(results.begin(), results.end());
+    mem[a][b][k] = *it;
+    return mem[a][b][k];
 }
